cbdbceecf5772aa02ca4f2840d52a8022b37e0f2
Scott Bailey <csb5t@virginia.edu>
2014-01-21 14:39:13 -0500

    added custom post types for games and moves

---
+++ functions.php
@@ -0,0 +1,39 @@
+<?php 
+
+add_action( 'init', 'create_post_type' );
+function create_post_type() {
+    register_post_type( 'ivanhoe_move',
+        array(
+            'labels' => array(
+                'name' => __( 'Moves' ),
+                'singular_name' => __( 'Move'),
+                'all_items' => __( 'All Moves' ),
+                'add_new_item' => __( 'Add New Move' )
+                ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array('slug' => 'moves'),
+            )
+
+        );
+}
+
+add_action( 'init', 'create_post_type' );
+function create_post_type() {
+    register_post_type( 'ivanhoe_game',
+        array(
+            'labels' => array(
+                'name' => __( 'Games' ),
+                'singular_name' => __( 'Game'),
+                'all_items' => __( 'All Games' ),
+                'add_new_item' => __( 'Add New Game' )
+                ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array('slug' => 'moves'),
+            )
+
+        );
+}
+
+?>
