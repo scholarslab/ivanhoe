816d3a41a04764c6caf7546aa9249be6f28d248b
Scott Bailey <csb5t@virginia.edu>
2014-01-21 16:23:48 -0500

    removed code registering games custom post type, as it broke wordpress and gave the wp-admin white screen of death.

--- functions.php
+++ functions.php
@@ -1,5 +1,4 @@
 <?php 
-
 add_action( 'init', 'create_post_type' );
 function create_post_type() {
     register_post_type( 'ivanhoe_move',
@@ -14,26 +13,6 @@ function create_post_type() {
             'has_archive' => true,
             'rewrite' => array('slug' => 'moves'),
             )
-
         );
 }
-
-add_action( 'init', 'create_post_type' );
-function create_post_type() {
-    register_post_type( 'ivanhoe_game',
-        array(
-            'labels' => array(
-                'name' => __( 'Games' ),
-                'singular_name' => __( 'Game'),
-                'all_items' => __( 'All Games' ),
-                'add_new_item' => __( 'Add New Game' )
-                ),
-            'public' => true,
-            'has_archive' => true,
-            'rewrite' => array('slug' => 'moves'),
-            )
-
-        );
-}
-
 ?>
