df54ab29dee57c41129c9338e81cc038679e7eae
Elizabeth Fox <ebf2px@virginia.edu>
2014-01-28 15:18:15 -0500

    Added header.php and footer.php Bothstill pretty basic.

---
+++ header.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+<!DOCTYPE html>
+
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="utf-8">
+
+    <title>Ghost Stories</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
