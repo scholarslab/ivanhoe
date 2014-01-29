51488206a34391ee5637afaaf3ac5dc342eeb3e4
Scott Bailey <csb5t@virginia.edu>
2014-01-29 12:42:19 -0500

    Changed files names for templates for single moves and single games to match wordpress requirements

---
+++ single-ivanhoe_game.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+<?php 
+
+$args = array ( 'post_type' = 'moves');
+$query = new WP_Query( $args );
+
+if ( $query->have_posts()) : while($query->have_posts()) : $query->the_post(); ?>
+<article>
+    <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+    <?php the_content(); ?>
+</article>
+
+<?php endwhile; else : ?>
+<p>OMG NO POSTS!!!!!</p>
+<?php endif; ?>
+
+
+<?php get_footer(); ?>
