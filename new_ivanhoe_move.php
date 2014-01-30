714e27cf4337dec7461c26977290071611f40a34
Scott Bailey <csb5t@virginia.edu>
2014-01-30 12:20:31 -0500

    Working front-end form for move submission
    On the new_ivanhoe_move.php file, I've created a working form that will accept title and content, and uses wp_insert_post to make that into a move that shows up in the dashboard and all. However, the parent id does not yet seem to be working, so we need to continue working on that.

--- new_ivanhoe_move.php
+++ new_ivanhoe_move.php
@@ -2,6 +2,28 @@
     require(dirname(__FILE__) . '/../../../wp-blog-header.php');
     get_header(); ?>
 
+<?php 
+
+if ( !empty( $_POST['title'] ) ){
+
+$ivanhoe_post_title = $_POST['title'];
+$ivanhoe_post_content = $_POST['content'];
+$ivanhoe_post_parent = $_POST['parent_id'];
+
+$post = array(
+	'post_content' => $ivanhoe_post_content,
+	'post_title' => $ivanhoe_post_title,
+	'post_status' => 'publish',
+	'post_type' => 'ivanhoe_move',
+	'post_parent' => $ivanhoe_post_parent
+);
+
+wp_insert_post( $post );
+
+} 
+
+?>     
+
 <?php
     $ivanhoe_game_id = $_GET['parent_post'];
     if ( current_user_can('edit_post', $ivanhoe_game_id))
@@ -10,7 +32,11 @@
 
 ?>
 
-here are some words.
-
+<form action="new_ivanhoe_move.php" method="post">
+	Title: <input type="text" name="title"><br>
+	Content: <input type="textarea" name="content"><br>
+	<input type="hidden" value="<?php $ivanhoe_game_id ?>" name="parent_id"><br>
+	<input type="submit" value="Submit">
+</form>
 
 <?php get_footer(); ?>
