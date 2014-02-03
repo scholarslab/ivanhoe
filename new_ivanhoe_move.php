938e50a52848a5c5362f93c7ca8d4f72ff9284b9
Jeremy Boggs <jeremy@clioweb.org>
2014-02-03 17:30:37 -0500

    Checks for a game ID before saving move.
    Updates move form to only insert a move post if there is a game ID
    passed as a $_GET parameter.
    
    Also slightly streamlines saving of data; Since we already have the
    parent ID, there's no need to set that in the POST again.

--- new_ivanhoe_move.php
+++ new_ivanhoe_move.php
@@ -2,46 +2,37 @@
 
 require('../../../wp-blog-header.php');
 
-if ( !empty( $_POST['post_title'] ) ) {
+// If parent_post is set as a query variable, use it, otherwise set to null.
+$ivanhoe_game_id = isset( $_GET['parent_post'] ) ? $_GET['parent_post'] : null;
+
+// If we have a game ID and a post title, insert a post.
+if ( $ivanhoe_game_id && !empty( $_POST['post_title'] ) ) {
 
     $ivanhoe_post_title = $_POST['post_title'];
     $ivanhoe_post_content = $_POST['post_content'];
-    $ivanhoe_post_parent = $_POST['parent_id'];
 
     $move = array(
         'post_content' => $ivanhoe_post_content,
         'post_title' => $ivanhoe_post_title,
         'post_status' => 'publish',
         'post_type' => 'ivanhoe_move',
-        'post_parent' => $ivanhoe_post_parent
+        'post_parent' => $ivanhoe_game_id
     );
 
     wp_insert_post( $move );
 
-    wp_redirect( get_permalink($ivanhoe_post_parent) );
+    wp_redirect( get_permalink($ivanhoe_game_id) );
     exit;
 
 }
 
 get_header();
 
-?>     
-
-<?php
-    $ivanhoe_game_id = $_GET['parent_post'];
-    $ivanhoe_parent_permalink = $_GET['parent_permalink'];
-    if ( current_user_can('edit_post', $ivanhoe_game_id))
-    	echo "I can edit this.";
-    echo $ivanhoe_parent_permalink;
- //   do_action('edit_post', 0);
-
 ?>
 
-<form action="new_ivanhoe_move.php" method="post">
+<form action="" method="post">
 	Title: <input type="text" name="post_title"><br>
 	Content: <?php wp_editor( '', "post_content"); ?><br>
-	<input type="hidden" value="<?php echo $ivanhoe_game_id ?>" name="parent_id"><br>
-	<input type="hidden" value="<?php echo $ivanhoe_parent_permalink ?>" name="ivanhoe_parent"><br>
 	<input type="submit" value="Submit">
 </form>
 
