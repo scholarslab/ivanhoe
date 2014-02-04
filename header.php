e6a668d0c967a40a2b94f4e9f9662ad7dfa98cef
Jeremy Boggs <jeremy@clioweb.org>
2014-02-04 12:44:34 -0500

    Output correct path to style sheet.
    Uses WP's get_stylesheet_uri function to correctly generate the path to
    our theme's style sheet.
    
    Refs #30.

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
 
     <title>Ghost Stories</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
 
 </head>
 
