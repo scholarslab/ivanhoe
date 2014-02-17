8ba3bd5953398c5713d44e3677b4d08bc646a512
Scott Bailey <csb5t@virginia.edu>
2014-02-17 10:31:08 -0500

    added working code to display the linked title of the source post of a move if it has a source post

--- single-ivanhoe_move.php
+++ single-ivanhoe_move.php
@@ -3,6 +3,18 @@
 <article>
     <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
     <?php the_content(); ?>
+		<?php 
+			$post_source = get_post_meta($post->ID, 'Ivanhoe Move Source', true);
+			$source_title = get_the_title($post_source);
+			$source_permalink = get_permalink($post_source);
+			if ( !empty($post_source) )
+			{ 
+				echo "Source post:"
+			?>
+			<a href="<?php echo $source_permalink ?>"><?php echo $source_title ?></a>
+			<?php
+			} 	
+		?>	
 </article>
 
 <?php endwhile; else : ?>
