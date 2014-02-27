6d6cefe4ba9f012842dbe3d6a7a6487fe956997a
Scott Bailey <csb5t@virginia.edu>
2014-02-27 11:24:12 -0500

    created the author template page and began to fill with some basic content, including a custom query that returns a list of the roles associated with the author. The next step here might be to provide the name of the game that role is associated with as well as a link to that game. Further step would be to return the most moves associated with each role

---
+++ author.php
@@ -0,0 +1,44 @@
+<?php get_header(); ?>
+
+<?php 
+	$curauth = ( isset( $_GET['author_name'] ) ) ? get_user_by( 'slug', $author_name ) : get_userdata( intval( $author ) );
+?>
+
+<h1>Hello <?php echo $curauth->first_name; ?></h1>	
+
+<p>Here's a little bit about yourself: <?php echo $curauth->user_description; ?></p>
+
+<!-- What we need to do is write a custom loop that pulls all the roles that the user has, and then also displays the title of the game that role is associated with -->
+
+<h3>Here are your roles:</h3>
+
+<?php 
+
+	$args = array 
+		(
+			'post_type' => 'ivanhoe_role',
+			'author_name' => $author_name
+		);
+
+	$author_role_query = new WP_Query($args);
+	
+	if ($author_role_query->have_posts()) : while($author_role_query->have_posts()) : $author_role_query->the_post();	
+?>
+
+<ul>
+	<li><?php the_title(); ?></li>
+</ul>	
+
+<?php
+
+	endwhile;
+	endif;
+
+?>	
+
+
+
+
+
+
+<?php get_footer(); ?>
