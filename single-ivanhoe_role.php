313b62ef51eb2fb06c570878d4ebf3dc72992405
Eliza Fox <ebf2px@virginia.edu>
2014-03-10 13:42:59 -0400

    Added Susy grids to multiple ivanhoe pages.

--- single-ivanhoe_role.php
+++ single-ivanhoe_role.php
@@ -1,6 +1,8 @@
 <?php get_header(); ?>
 <?php if (have_posts()) : while(have_posts()) : the_post(); ?>
-<article>
+
+<article class="role">
+
     <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
     
     <?php the_content(); ?>	
