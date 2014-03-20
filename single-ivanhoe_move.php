6bcc5986c6420d97af5e2a1d25683543bd4bd2ad
Eliza Fox <ebf2px@virginia.edu>
2014-03-20 11:46:11 -0400

    Made changes to address #72

--- single-ivanhoe_move.php
+++ single-ivanhoe_move.php
@@ -3,13 +3,33 @@
 <article class="single-move">
 
     <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-    <?php the_content();
+	<div id="game-data">
 
-		ivanhoe_get_move_source( $post );	
-		ivanhoe_get_move_responses( $post );
+ <p><?php echo get_the_title($post->post_parent); ?></p>
+ <p>Written by: <?php the_author_posts_link(); ?></p>
+ <p>Posted on: <?php the_date(); ?></p>
+
+	</div>
+
+    <div id="moves">
+
+    <?php the_content(); ?>
+
+    <?php ivanhoe_get_move_source( $post ); ?>
+    <?php ivanhoe_get_move_responses( $post ); ?>  
+
+
+<!--     <div id="eliza">
+
+    <?php ivanhoe_get_move_source( $post ); ?>
+
+</div>
+    <div id="zach">
+
+    <?php ivanhoe_get_move_responses( $post );
 	?>	
 
-	<p>Written by: <?php the_author_posts_link(); ?></p>
+</div> -->
 
 	<a href="<?php echo ivanhoe_response_form_url( $post ); ?>" class="button">Respond to this move</a>
 
@@ -24,5 +44,7 @@
 <?php endwhile; else : ?>
 <p>No one has made a move yet in this game.  Make the first move!</p>
 <?php endif; ?>
+
+</div>
 
 <?php get_footer(); ?>
