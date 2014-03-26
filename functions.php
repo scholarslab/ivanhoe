1aa7fd692806e71ff56a673ec4330b6ef0267516
Scott Bailey <csb5t@virginia.edu>
2014-03-26 13:33:23 -0400

    added function to try to get author link into nav menu. to be fixed.

--- functions.php
+++ functions.php
@@ -130,12 +130,16 @@ function ivanhoe_make_menus() {
 
     $menu_name = 'ivanhoe_default';
 
+    //Gets current user info
+    // global $current_user;
+    // get_currentuserinfo();
+
     // Check if the menu exists
     $menu_exists = wp_get_nav_menu_object( $menu_name );
 
     // If it doesn't exist, let's create it.
     if( !$menu_exists){
-        $menu_id = wp_create_nav_menu($menu_name);
+        $menu_id = wp_create_nav_menu($menu_name);    
 
         // Set up default menu items
         wp_update_nav_menu_item($menu_id, 0, array(
@@ -147,6 +151,11 @@ function ivanhoe_make_menus() {
         wp_update_nav_menu_item($menu_id, 0, array(
             'menu-item-title' =>  __('Games'),
             'menu-item-url' => get_post_type_archive_link('ivanhoe_game'),
+            'menu-item-status' => 'publish'));
+
+        wp_update_nav_menu_item($menu_id, 0, array(
+            'menu-item-title' => __('Profile'),
+            'menu-item-url' => home_url() . '/author/' . get_the_author_meta( 'user_login', wp_get_current_user()->ID ),
             'menu-item-status' => 'publish'));
     }
 }
