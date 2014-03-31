25241da65aa56cf615093ad6ef2ba419522d0d38
Eliza Fox <ebf2px@virginia.edu>
2014-03-31 11:29:37 -0400

    Reduced font size for game titles.

--- archive-ivanhoe_game.php
+++ archive-ivanhoe_game.php
@@ -3,7 +3,7 @@
 <?php
 if ( have_posts()) : while(have_posts()) : the_post(); ?>
 <article class="game">
-    <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php the_excerpt(); ?>
 </article>
 
