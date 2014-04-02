fdeb58666b85790f75de305e46e3c86eb5f02354
Eliza Fox <ebf2px@virginia.edu>
2014-04-02 13:51:14 -0400

    Reduces profile image size, floats to left.

--- single-ivanhoe_role.php
+++ single-ivanhoe_role.php
@@ -5,7 +5,7 @@
 
     <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 
-    <?php the_post_thumbnail(); ?>
+    <?php the_post_thumbnail('medium'); ?>
     
     <?php the_content(); ?>	
 
