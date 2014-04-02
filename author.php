fa2a02241ae4354016f36ded9793274992cac7ca
Jeremy Boggs <jeremy@clioweb.org>
2014-04-02 15:08:58 -0400

    Markup and style changes for author page.
    Adds a bit more structure to the author page. Specifically, this wraps each role
    in an article tag, and using a top-level heading for the role name itself. It
    also includes the role thumbnail.
    
    Lastly, this implements a two-column layout for the profile page, with the left
    column featuring a list of user roles, and the right column showing the user's
    description if one exists.

--- author.php
+++ author.php
@@ -7,11 +7,13 @@
 <article>
 <h1><?php echo $curauth->display_name; ?></h1>
 
-<p>Here's a little bit about yourself: <?php echo $curauth->user_description; ?></p>
-
+<?php if ($description = $curauth->user_description) : ?>
+<div class="description">
+<h2>Description</h2>
+<?php echo wpautop($description); ?>
+</div>
+<?php endif; ?>
 <!-- What we need to do is write a custom loop that pulls all the roles that the user has, and then also displays the title of the game that role is associated with -->
-
-<h3>Here are your roles:</h3>
 
 <?php 
 
@@ -23,13 +25,18 @@
 
 	$author_role_query = new WP_Query($args);
 	
-	if ($author_role_query->have_posts()) : while($author_role_query->have_posts()) : $author_role_query->the_post();	
-?>
+    if ($author_role_query->have_posts()) : ?>
 
+<h2>Roles</h2>
 
-<p><strong>Game title: </strong><a href="<?php echo get_permalink( $post->post_parent ); ?>"><?php echo get_the_title( $post->post_parent ); ?></a></p>
-<p><strong>Role: </strong><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
-	
+<div class="roles">
+
+<?php while($author_role_query->have_posts()) : $author_role_query->the_post();	?>
+
+<article class="role">
+    <?php the_post_thumbnail('thumbnail'); ?>
+    <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+    <p class="game"><strong>Game: </strong><a href="<?php echo get_permalink( $post->post_parent ); ?>"><?php echo get_the_title( $post->post_parent ); ?></a></p>
 
 <?php 
 	$args = array
@@ -42,22 +49,20 @@
 
 	$moves_per_role_query = new WP_Query( $args );
 
-	if ($moves_per_role_query->have_posts()) : while($moves_per_role_query->have_posts()) : $moves_per_role_query->the_post();
+    if ($moves_per_role_query->have_posts()) : ?>
 
-?>
+    <ul class="moves">
+    <?php while($moves_per_role_query->have_posts()) : $moves_per_role_query->the_post(); ?>
 
-<ul>
-	<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-</ul>
+        <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
 
-<?php	
+    <?php endwhile; ?>
+    </ul>
 
-	endwhile;
-	endif;
+    <?php endif; ?>
 
-	wp_reset_postdata();
-
-?>
+<?php wp_reset_postdata(); ?>
+</article>    
 
 <?php
 
@@ -65,7 +70,7 @@
 	endif;
 
 ?>	
-
+</div>
 </article>
 
 
