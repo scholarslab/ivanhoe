18643128598bf4a05cefaa50cd934cc093c3cdce
Veronica Ikeshoji-Orlati <vai2fc@virginia.edu>
2014-04-08 15:02:55 -0400

    admin can now change move source in dashboard.

--- functions.php
+++ functions.php
@@ -155,10 +155,88 @@ function ivanhoe_move_source()
     add_meta_box(
         'ivanhoe_move_source',
         __('Source for:'),
-        'ivanhoe_source_meta_box',
+        'ivanhoe_move_source_meta_box',
         'ivanhoe_move'
     );
 }
+
+/**
+ * Prints the box content.
+ * 
+ * @param WP_Post $post The object for the current post/page.
+ */
+function ivanhoe_move_source_meta_box( $post ) {
+
+    // Add an nonce field so we can check for it later.
+    wp_nonce_field( 'ivanhoe_move_source_meta_box', 'ivanhoe_move_source_meta_box_nonce' );
+
+    /*
+     * Use get_post_meta() to retrieve an existing value
+     * from the database and use the value for the form.
+     */
+    $value = get_post_meta( $post->ID, 'Ivanhoe Move Source', true );
+
+    echo '<label for="ivanhoe_move_source">';
+    __( 'Source for:' );
+    echo '</label> ';
+    echo '<input type="text" id="ivanhoe_move_source" name="ivanhoe_move_source" value="' . esc_attr( $value ) . '" size="25" />';
+}
+
+/**
+ * When the post is saved, saves our custom data.
+ *
+ * @param int $post_id The ID of the post being saved.
+ */
+function ivanhoe_move_source_save_meta_box_data( $post_id ) {
+
+    /*
+     * We need to verify this came from our screen and with proper authorization,
+     * because the save_post action can be triggered at other times.
+     */
+
+    // Check if our nonce is set.
+    if ( ! isset( $_POST['ivanhoe_move_source_meta_box_nonce'] ) ) {
+        return;
+    }
+
+    // Verify that the nonce is valid.
+    if ( ! wp_verify_nonce( $_POST['ivanhoe_move_source_meta_box_nonce'], 'ivanhoe_move_source_meta_box' ) ) {
+        return;
+    }
+
+    // If this is an autosave, our form has not been submitted, so we don't want to do anything.
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+        return;
+    }
+
+    // Check the user's permissions.
+    if ( isset( $_POST['post_type'] ) && 'ivanhoe_move' == $_POST['post_type'] ) {
+
+        if ( ! current_user_can( 'edit_page', $post_id ) ) {
+            return;
+        }
+
+    } else {
+
+        if ( ! current_user_can( 'edit_post', $post_id ) ) {
+            return;
+        }
+    }
+
+    /* OK, its safe for us to save the data now. */
+    
+    // Make sure that it is set.
+    if ( ! isset( $_POST['ivanhoe_move_source'] ) ) {
+        return;
+    }
+
+    // Sanitize user input.
+    $my_data = sanitize_text_field( $_POST['ivanhoe_move_source'] );
+
+    // Update the meta field in the database.
+    update_post_meta( $post_id, 'Ivanhoe Role ID', $my_data );
+}
+add_action( 'save_post', 'ivanhoe_move_source_save_meta_box_data' );
 
 /**
  * Function for getting the metadata for the post(s) to which the current move responds
