8db353d3cf9443b592ac93e6be547eff47e08e9b
Veronica Ikeshoji-Orlati <vai2fc@virginia.edu>
2014-04-08 14:43:56 -0400

    now admins can save metadata for roles on RJ entries.

--- functions.php
+++ functions.php
@@ -188,6 +188,83 @@ function ivanhoe_role_for_journal()
 
 add_action('add_meta_boxes', 'ivanhoe_move_source', 'ivanhoe_move_response', 'ivanhoe_role_for_journal');
 
+/**
+ * Prints the box content.
+ * 
+ * @param WP_Post $post The object for the current post/page.
+ */
+function ivanhoe_role_id_meta_box( $post ) {
+
+    // Add an nonce field so we can check for it later.
+    wp_nonce_field( 'ivanhoe_role_id_meta_box', 'ivanhoe_role_id_meta_box_nonce' );
+
+    /*
+     * Use get_post_meta() to retrieve an existing value
+     * from the database and use the value for the form.
+     */
+    $value = get_post_meta( $post->ID, 'Ivanhoe Role ID', true );
+
+    echo '<label for="ivanhoe_role_for_journal">';
+    __( 'Journal Entry For:' );
+    echo '</label> ';
+    echo '<input type="text" id="ivanhoe_role_id" name="ivanhoe_role_id" value="' . esc_attr( $value ) . '" size="25" />';
+}
+
+/**
+ * When the post is saved, saves our custom data.
+ *
+ * @param int $post_id The ID of the post being saved.
+ */
+function ivanhoe_role_id_save_meta_box_data( $post_id ) {
+
+    /*
+     * We need to verify this came from our screen and with proper authorization,
+     * because the save_post action can be triggered at other times.
+     */
+
+    // Check if our nonce is set.
+    if ( ! isset( $_POST['ivanhoe_role_id_meta_box_nonce'] ) ) {
+        return;
+    }
+
+    // Verify that the nonce is valid.
+    if ( ! wp_verify_nonce( $_POST['ivanhoe_role_id_meta_box_nonce'], 'ivanhoe_role_id_meta_box' ) ) {
+        return;
+    }
+
+    // If this is an autosave, our form has not been submitted, so we don't want to do anything.
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+        return;
+    }
+
+    // Check the user's permissions.
+    if ( isset( $_POST['post_type'] ) && 'ivanhoe_role_journal' == $_POST['post_type'] ) {
+
+        if ( ! current_user_can( 'edit_page', $post_id ) ) {
+            return;
+        }
+
+    } else {
+
+        if ( ! current_user_can( 'edit_post', $post_id ) ) {
+            return;
+        }
+    }
+
+    /* OK, its safe for us to save the data now. */
+    
+    // Make sure that it is set.
+    if ( ! isset( $_POST['ivanhoe_role_id'] ) ) {
+        return;
+    }
+
+    // Sanitize user input.
+    $my_data = sanitize_text_field( $_POST['ivanhoe_role_id'] );
+
+    // Update the meta field in the database.
+    update_post_meta( $post_id, 'Ivanhoe Role ID', $my_data );
+}
+add_action( 'save_post', 'ivanhoe_role_id_save_meta_box_data' );
 
 /**
  * Create a custom nav menu for the theme.
@@ -661,3 +738,4 @@ function restrict_admin_with_redirect()
 }
 
 add_action( 'admin_init', 'restrict_admin_with_redirect' );
+
