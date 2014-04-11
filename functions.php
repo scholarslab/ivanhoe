950428705031168692ef8bc0087b792cad8a8dcc
Veronica Ikeshoji-Orlati <vai2fc@virginia.edu>
2014-04-11 12:03:05 -0400

    modified the_excerpt to show 'Read More' and link to post.

--- functions.php
+++ functions.php
@@ -831,3 +831,11 @@ function restrict_admin_with_redirect()
 
 add_action( 'admin_init', 'restrict_admin_with_redirect' );
 
+/**
+ * Creates a "Read More" link to post at the end of the_excerpt.
+ */
+
+function new_excerpt_more( $more ) {
+    return ' <a class="read-more" href="'. get_permalink( get_the_ID() ) . '">' . __('Read More', 'your-text-domain') . '</a>';
+}
+add_filter( 'excerpt_more', 'new_excerpt_more' );
