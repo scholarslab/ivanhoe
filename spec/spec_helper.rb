64cd10f5bb5af00027ff8a76bc1bf63ed9a67e72
Eric Rochester <erochest@virginia.edu>
2014-04-28 18:40:13 +0000

    Patch wp config for testing.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,14 +7,9 @@ require 'capybara-webkit'
 
 Dotenv.load
 
-WP_CONFIG   = '../../../wp-config.php'
 
-
-ADAPTER = ENV.fetch('ADAPTER', 'mysql2')
-
-require ADAPTER
-
-# You need to set up this database in postgres/mysql.
+# You need to set up this database in mysql.
+WP_CONFIG   = ENV.fetch('WP_CONFIG',   '../../../wp-config.php')
 
 DB_HOST     = ENV.fetch('DB_HOST',     'localhost')
 DB_USER     = ENV.fetch('DB_USER',     'ivanhoe')
@@ -22,7 +17,7 @@ DB_PASSWORD = ENV.fetch('DB_PASSWORD', 'ivanhoe')
 DB_NAME     = ENV.fetch('DB_NAME',     'test_ivanhoe')
 URL_BASE    = ENV.fetch('URL_BASE',    'http://localhost:8888/ivanhoe')
 
-DB_DUMP     = "./spec/dumps/ivanhoe-#{ENV.fetch('ADAPTER', 'mysql2')}.sql"
+DB_DUMP     = "./spec/dumps/ivanhoe.sql"
 
 
 Capybara.default_driver    = :webkit
@@ -30,31 +25,31 @@ Capybara.javascript_driver = :webkit
 
 RSpec.configure do |config|
 
-  #config.before(:suite) do |ex|
-  #Dir.mkdir('./tmp') unless Dir.exists?('./tmp')
+  # Patch wp config for testing.
+  config.before(:suite) do |ex|
+    Dir.mkdir('./tmp') unless Dir.exists?('./tmp')
 
-  #FileUtils.cp(WP_CONFIG, './tmp/wp-config.php')
-  #File.open(WP_CONFIG, mode='w') do |f|
-  #IO.readlines('./tmp/wp-config.php').each do |line|
-  #if line.start_with?("define('DB_NAME'")
-  #line = "define('DB_NAME',     '#{DB_NAME}');\n# #{line}"
-  #elsif line.start_with?("define('DB_USER'")
-  #line = "define('DB_USER',     '#{DB_USER}');\n# #{line}"
-  #elsif line.start_with?("define('DB_PASSWORD'")
-  #line = "define('DB_PASSWORD', '#{DB_PASSWORD}');\n# #{line}"
-  #elsif line.start_with?("define('DB_HOST'")
-  #line = "define('DB_HOST',     '#{DB_HOST}');\n# #{line}"
-  #end
+    FileUtils.cp(WP_CONFIG, './tmp/wp-config.php')
+    File.open(WP_CONFIG, mode='w') do |f|
+      IO.readlines('./tmp/wp-config.php').each do |line|
+        if line.start_with?("define('DB_NAME'")
+          line = "define('DB_NAME',     '#{DB_NAME}');\n# #{line}"
+        elsif line.start_with?("define('DB_USER'")
+          line = "define('DB_USER',     '#{DB_USER}');\n# #{line}"
+        elsif line.start_with?("define('DB_PASSWORD'")
+          line = "define('DB_PASSWORD', '#{DB_PASSWORD}');\n# #{line}"
+        elsif line.start_with?("define('DB_HOST'")
+          line = "define('DB_HOST',     '#{DB_HOST}');\n# #{line}"
+        end
 
-  #f.write(line)
-  #end
-  #end
-  #end
+        f.write(line)
+      end
+    end
+  end
 
-
-  #config.after(:suite) do |ex|
-  #FileUtils.cp('./tmp/wp-config.php', WP_CONFIG)
-  #end
+  config.after(:suite) do |ex|
+    FileUtils.cp('./tmp/wp-config.php', WP_CONFIG)
+  end
 
   #config.before(:suite) do |ex|
 
