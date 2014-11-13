e221478ce176d63ea2e1a2947480e773930fb4f8
csbailey5t <csb5t@virginia.edu>
2014-11-13 13:03:01 -0500

    spacing

--- spec/games_spec.rb
+++ spec/games_spec.rb
@@ -25,6 +25,11 @@ describe "Transactions", :type => :feature, :js => true  do
         make_game
         within('.ivanhoe_game') { expect(page).to have_selector('h2') }
       end
+
+      it 'game creation page has errors' do
+        click_link 'Games'
+        expect(page).not_to have_errors
+      end
 end
 
 describe "Games View", :type => :feature, :js => true  do
