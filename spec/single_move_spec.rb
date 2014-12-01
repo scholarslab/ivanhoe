691039219a03b1558b35f994c979e2ff5e99600b
csbailey5t <csb5t@virginia.edu>
2014-12-01 13:10:23 -0500

    further tests for single moves

--- spec/single_move_spec.rb
+++ spec/single_move_spec.rb
@@ -11,12 +11,12 @@ describe 'Single Move View', :type => :feature, :js => true do
     :game_description => Faker::Lorem.paragraphs(rand(1..3))
     }
 
-      def login
-    click_link('Log in')
-    fill_in 'Username', with: 'admin'
-    fill_in 'Password', with: 'admin'
-    click_button 'Log In'
-  end
+    def login
+        click_link('Log in')
+        fill_in 'Username', with: 'admin'
+        fill_in 'Password', with: 'admin'
+        click_button 'Log In'
+    end
 
   def make_game
     click_link 'Make a Game'
@@ -61,10 +61,13 @@ describe 'Single Move View', :type => :feature, :js => true do
         first('.game-title a').click
         make_role
         rand(2..5).times { make_a_move }
-        first('#moves a').click
     end
 
     describe 'with no responses' do
+
+        before do
+            first('#moves a').click
+        end
 
         it 'has a move article' do
           expect(page).to have_selector('.single-move')
@@ -100,15 +103,87 @@ describe 'Single Move View', :type => :feature, :js => true do
             end
         end
 
+        it 'has a respond button' do
+          expect(page).to have_link('Respond')
+        end
+
         it 'has the game description block' do
             expect(page).to have_selector('.game-description')
         end
 
     end
 
+    describe 'with responses' do
 
+        before do
+            first('#moves a').click
+            respond_to_move
+        end
+
+        describe "the response move" do
+
+            before do
+                first('#moves a').click
+            end
+
+            it 'has move source section' do
+                expect(page).to have_selector('.discussion-source')
+            end
+
+            it 'has source label' do
+                within('.discussion-source') do
+                    expect(page).to have_selector('h3')
+                end
+            end
+
+            it 'has list of source moves' do
+                within('.discussion-source') do
+                    expect(page).to have_selector('ul')
+                end
+            end
+
+            it 'has a linked move title for the source' do
+              within('.discussion-source') do
+                  expect(page).to have_selector('ul li a')
+              end
+            end
+
+        end
+
+        describe "the source move" do
+
+            before do
+                moves = page.all('#moves article')
+                second_move_title = moves[1].find('h1 a', match: :first)
+                second_move_title.click
+            end
+
+            it 'has move response section' do
+                expect(page).to have_selector('.discussion-response')
+            end
+
+            it 'has responses label' do
+                within('.discussion-response') do
+                    expect(page).to have_selector('h3')
+                end
+            end
+
+            it 'has list of response moves' do
+                within('.discussion-response') do
+                    expect(page).to have_selector('ul')
+                end
+            end
+
+            it 'has a linked move title for the response' do
+              within('.discussion-response') do
+                  expect(page).to have_selector('ul li a')
+              end
+            end
+
+        end
+
+    end
 
   end
-
 
 end
