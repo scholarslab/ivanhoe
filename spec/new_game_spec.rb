df300dd427a45d6961d6bd4282aa9f8171b7a8f8
csbailey5t <csb5t@virginia.edu>
2014-12-02 10:54:38 -0500

    tests for new game view

--- spec/new_game_spec.rb
+++ spec/new_game_spec.rb
@@ -40,3 +40,51 @@ def respond_to_move
     tiny_mce_fill_in('post_rationale', :with => Faker::Lorem.paragraphs(rand(3..10)).join('<p>'))
     click_button 'Save'
 end
+
+describe 'Make a Game View', :type => :feature, :js => true do
+
+    before(:each) do
+        visit(URL_BASE)
+        click_link('Games')
+        login
+        click_link('Make a Game')
+    end
+
+    it 'has the Make a Game header' do
+      expect(page).to have_content('Make a Game')
+    end
+
+    it 'has a game title label' do
+      expect(page).to have_content('Game Title')
+    end
+
+    it 'has a game title field' do
+      expect(page).to have_field('post_title', :type => 'text')
+    end
+
+    it 'has a game thumbnail label' do
+      expect(page).to have_content('Game Thumbnail')
+    end
+
+    it 'has a game thumbnail input' do
+      expect(page).to have_field('post_thumbnail', :type => 'file')
+    end
+
+    it 'has a game description label' do
+      expect(page).to have_content('Game Description')
+    end
+
+    it 'has the Add Media button' do
+      expect(page).to have_selector('#insert-media-button')
+    end
+
+    it 'has a game description input' do
+      expect(page).to have_selector('#wp-post_content-wrap')
+      # not sure if this is the best selector for this
+    end
+
+    it 'has a save button' do
+      expect(page).to have_selector('input[type=submit][value=Save]')
+    end
+
+end
