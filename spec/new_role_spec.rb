d728ae3f49ecc3c5212a118b2961367aad5e0e75
csbailey5t <csb5t@virginia.edu>
2014-12-02 11:10:52 -0500

    tests for new role view

--- spec/new_role_spec.rb
+++ spec/new_role_spec.rb
@@ -40,3 +40,63 @@ def respond_to_move
     tiny_mce_fill_in('post_rationale', :with => Faker::Lorem.paragraphs(rand(3..10)).join('<p>'))
     click_button 'Save'
 end
+
+describe 'Make a Role View', :type => :feature, :js => true do
+
+    before(:each) do
+        visit(URL_BASE)
+        click_link('Games')
+        login
+        make_game
+        first('.game-title a').click
+        click_link('Make a Role!')
+    end
+
+    it 'has the Make a Role header' do
+      expect(page).to have_content('Make a Role')
+    end
+
+    it 'has a role name label' do
+      expect(page).to have_content('Role Name')
+    end
+
+    it 'has a role name field' do
+      expect(page).to have_field('post_title', :type => 'text')
+    end
+
+    it 'has a role thumbnail label' do
+      expect(page).to have_content('Role Thumbnail')
+    end
+
+    it 'has a role thumbnail input' do
+      expect(page).to have_field('post_thumbnail', :type => 'file')
+    end
+
+    it 'has a role description label' do
+      expect(page).to have_content('Role Description')
+    end
+
+    it 'has the Add Media button' do
+      expect(page).to have_selector('#insert-media-button')
+    end
+
+    it 'has a role description input' do
+      expect(page).to have_selector('#wp-post_content-wrap')
+      # not sure if this is the best selector for this
+    end
+
+    it 'has a save button' do
+      expect(page).to have_selector('input[type=submit][value=Save]')
+    end
+
+    it 'indicates the game with which the role is connected' do
+      expect(page).to have_selector('.new-ivanhoe-meta')
+    end
+
+    it 'has a link to the game with which the role is connected' do
+      within('.new-ivanhoe-meta') do
+        expect(page).to have_selector('a')
+      end
+    end
+
+end
