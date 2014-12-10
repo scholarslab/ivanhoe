f1962fb63757e7f3b41d06d86796ca6b4aed7eed
arboyd <arboyd173@gmail.com>
2014-12-10 15:58:02 -0500

    updated switcher.js not to do anything when the activated letter is hovered over

--- js/switcher.js
+++ js/switcher.js
@@ -1,17 +1,17 @@
 $(function(){
 
-function testAlert() {
-    alert('uuuugh');
-}
 
 
 function styleSwitch(){
-    $("body").fadeOut(1000);
     var letter = $(this).attr('id');
+    if ($("#switch").attr("href") !== "css/"+ letter + ".css"){
+    $("body").fadeOut(1000);
     setTimeout(function(){
         $("#switch").attr("href", "css/"+ letter + ".css");
         $("body").fadeIn(1000);
     }, 1000);
+    
+    }
 }
 
 
