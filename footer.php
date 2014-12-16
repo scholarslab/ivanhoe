5e481fafda2626336a18fea03291b9691e311398
csbailey5t <csb5t@virginia.edu>
2014-12-16 14:35:19 -0500

    formatting

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
 </main>
 
 <footer role="contentinfo">
-<a href="http://ivanhoe.scholarslab.org" class="ivanhoelink"><img class="aligncenter" src="<?php echo get_template_directory_uri() . '/ivanhoelogo.png'; ?>" alt="Ivanhoe"></a>
-<?php wp_footer(); ?>
+    <a href="http://ivanhoe.scholarslab.org" class="ivanhoelink"><img class="aligncenter" src="<?php echo get_template_directory_uri() . '/ivanhoelogo.png'; ?>" alt="Ivanhoe"></a>
+    <?php wp_footer(); ?>
 </footer>
 
 </div>
--- header.php
+++ header.php
@@ -5,39 +5,38 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
 
-   <title><?php echo wp_title( '|', true, 'right' ); ?><?php echo get_bloginfo( 'name' ); ?></title>
+    <title><?php echo wp_title( '|', true, 'right' ); ?><?php echo get_bloginfo( 'name' ); ?></title>
 
-   <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,400,700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="<?php echo get_template_directory_uri() . '/stylesheets/screen.css'; ?>">
 
-<?php
-if ( is_singular() && get_option( 'thread_comments' ) )
-	wp_enqueue_script( 'comment-reply' );
-?>
+    <?php
+    if ( is_singular() && get_option( 'thread_comments' ) )
+       wp_enqueue_script( 'comment-reply' );
+   ?>
 
-<?php wp_head(); ?>
+   <?php wp_head(); ?>
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 
 </head>
 
 <body <?php echo body_class(); ?>>
 
-<div id="wrap">
+    <div id="wrap">
 
-<header role="banner">
+        <header role="banner">
 
-<h1><a href="<?php echo home_url(); ?>"><?php echo get_bloginfo('site_title'); ?></a></h1>
-<?php if($description = get_bloginfo('description')):?>
-<p><?php echo $description ?></p>
-<?php endif; ?>
+            <h1><a href="<?php echo home_url(); ?>"><?php echo get_bloginfo('site_title'); ?></a></h1>
+            <?php
+                if($description = get_bloginfo('description')):?>
+                <p><?php echo $description ?></p>
+            <?php endif; ?>
 
-<?php
+            <?php
+            wp_nav_menu( array( 'fallback_cb' => 'ivanhoe_page_menu', 'theme_location' => 'header', 'container' => 'nav' ) );
+            ?>
 
-    wp_nav_menu( array( 'fallback_cb' => 'ivanhoe_page_menu', 'theme_location' => 'header', 'container' => 'nav' ) );
+        </header>
 
-?>
-
-</header>
-
-<main>
+        <main>
