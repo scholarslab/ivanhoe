fdf8601d20a60b68c1674ab35acda7ec7de40419
Eric Rochester <erochest@virginia.edu>
2015-02-11 15:28:51 -0500

    Mis-stream rewriting make_game.

--- spec/support/application_helpers.rb
+++ spec/support/application_helpers.rb
@@ -54,11 +54,50 @@ module ApplicationHelper
     click_button 'Log In'
   end
 
+  # Timings:
+  # - 0.552375
+  # - 0.516728
+  # - 0.551325
+  # mean = 0.540142667
   def make_game
+    db_dump
+    # results = @cxn.query("SELECT MAX(ID)+1 AS row_id FROM wp_posts;")
+    # row_id = results.to_a.first['row_id']
+
+    # # TODO: also call @cxn.escape on all of these (and the guid)
+    # # TODO: Am I sure I don't want to just use sequel?
+    # guid = "#{BASE_URL}/?post_type=ivanhoe_game&p=#{row_id}"
+    # post_author = nil
+    # post_content = nil
+    # post_content_filtered = nil
+
+    # HEREIAM: the data in this line appears to be the autosave data. I've
+    # added the sleep below, so hopefully re-running it should now output the
+    # complete saved entry.
+
+    # @cxn.query(<<END
+      # INSERT INTO wp_posts
+        # (ID,comment_count,comment_status,guid,menu_order,ping_status,pinged,
+        # post_author,post_content,post_content_filtered,
+        # post_date,post_date_gmt,post_excerpt,post_mime_type,
+        # post_modified,post_modified_gmt,
+        # post_name,
+        # post_parent,post_password,post_status,post_title,post_type,to_ping)
+        # VALUES
+        # (#{row_id},0,'open',#{guid},0,'open','',
+        # #{post_author},#{post_content},#{post_content_filtered},
+        # #{now},#{now_gmt},'','',
+        # #{now},#{now_gmt},'',0,'',auto-draft,Auto Draft,ivanhoe_game,''
+# END
+# )
+
     click_link 'Make a Game'
     fill_in 'post_title', :with => Faker::Lorem.words(rand(2..8)).join(' ')
     tiny_mce_fill_in_post_content('post_content', :with => Faker::Lorem.paragraphs(rand(3..10)).join('<p>'))
     click_button 'Save'
+
+    sleep 0.5
+    db_dump
   end
 
   def make_role
