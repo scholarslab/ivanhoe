11f86425cfd46a06e182e2e80291211ae73ec657
csbailey5t <csb5t@virginia.edu>
2015-03-04 10:18:01 -0500

    Creates helper function for responding to multiple moves a random number of times.

--- spec/multisource_spec.rb
+++ spec/multisource_spec.rb
@@ -16,32 +16,28 @@ before(:each) do
      rand(2..3).times { make_game }
 end
 
-describe "a view with a response with multiple sources"
+describe "a move"
 
     before do
       first('.game-link').click
       make_role
-      2.times { make_a_move }
-      @moves = page.all('#moves article')
-      first_move_button = @moves[0].find('.new_source', match: :first)
-      second_move_button = @moves[1].find('.new_source', match: :first)
-      first_move_button.click
-      second_move_button.click
-      click_button 'Respond'
-      fill_in 'post_title', :with => Faker::Lorem.words(rand(2..8)).join(' ')
-      tiny_mce_fill_in_post_content('post_content', :with => Faker::Lorem.paragraphs(rand(3..10)).join('<p>'))
-      tiny_mce_fill_in_post_rationale('post_rationale', :with => Faker::Lorem.paragraphs(rand(3..10)).join('<p>'))
-      click_button 'Save'
+      @n = rand(2...10)
+      @n.times { make_a_move }
+      respond_to_multiple_moves
     end
 
-    it 'should fail' do
+    it 'should list multiple sources in the source column' do
       source_block = first('#moves article .game-discussion-source')
       within(source_block) do
-        expect(find('ul')).to have_selector('li', count: 2)
+        expect(find('ul')).to have_selector('li', count: @n)
       end
     end
 
-    # need to abstract this to make an actual respond to multiple move helper function
-    # Would create 10 moves...click random number of moves from moves array
+
+    # create random number n
+    # create n moves
+    # respond to a random number of moves between 2 and n
+    # that means clicking .new_source of n different moves
+    # check for that number of list items in the it block
 
 end
--- spec/support/application_helpers.rb
+++ spec/support/application_helpers.rb
@@ -49,7 +49,7 @@ module ApplicationHelper
     click_button 'Save'
   end
 
-# This respond to move function primarily covers the single_ivanhoe_move view
+# This respond to move function primarily covers the single-ivanhoe_move view
   def respond_to_move
     click_link('Respond')
     fill_in 'post_title', :with => Faker::Lorem.words(rand(2..8)).join(' ')
@@ -58,7 +58,7 @@ module ApplicationHelper
     click_button 'Save'
   end
 
-# The following function covers responding to a move from the single_ivanhoe_game page.
+# This function covers responding to a move from the single-ivanhoe_game page.
   def main_page_respond_to_move
     first('.new_source').click
     click_button('Respond')
@@ -68,8 +68,23 @@ module ApplicationHelper
     click_button 'Save'
   end
 
+# This function covers responding to multiple moves from the single-ivanhoe_game page
   def respond_to_multiple_moves
-
+    moves = page.all('#moves article')
+    # first_move_button = moves[0].find('.new_source', match: :first)
+    # second_move_button = moves[1].find('.new_source', match: :first)
+    # first_move_button.click
+    # second_move_button.click
+    moves.each do |move|
+      within move do
+        find('.new_source').click
+      end
+    end
+    click_button 'Respond'
+    fill_in 'post_title', :with => Faker::Lorem.words(rand(2..8)).join(' ')
+    tiny_mce_fill_in_post_content('post_content', :with => Faker::Lorem.paragraphs(rand(3..10)).join('<p>'))
+    tiny_mce_fill_in_post_rationale('post_rationale', :with => Faker::Lorem.paragraphs(rand(3..10)).join('<p>'))
+    click_button 'Save'
   end
 
 end
