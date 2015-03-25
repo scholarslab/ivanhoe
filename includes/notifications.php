70109101f19b96d26b3460fcafa7a6337af1ee03
Jeremy Boggs <jeremy@clioweb.org>
2015-03-25 11:28:38 -0400

    Adds email notification for any move.
    Adds an option for individual users to receive an email notification for any
    move on a game in which they're participating.

--- includes/notifications.php
+++ includes/notifications.php
@@ -6,11 +6,21 @@ add_action( 'edit_user_profile', 'ivanhoe_display_notifications_options' );
 
 function ivanhoe_display_notifications_options( $user ) {
 
+    $notification_all_moves = get_the_author_meta( 'notification_all_moves', $user->ID );
     $notification_response_moves = get_the_author_meta( 'notification_response_moves', $user->ID );
 
 ?>
   <h3><?php _e("Ivanhoe Notifications", "ivanhoe"); ?></h3>
   <table class="form-table">
+    <tr>
+      <th><label for="notification_all_moves"><?php _e("All moves"); ?></label></th>
+      <td>
+        <input type="checkbox" name="notification_all_moves" id="notification_all_moves" value="true"
+        <?php if ($notification_all_moves == true):?> checked="checked"<?php endif; ?>
+ /><br />
+        <span class="description"><?php _e("Check if you wish to receive an email every time someone makes a move on the games your playing."); ?></span>
+    </td>
+    </tr>
     <tr>
       <th><label for="notification_responses"><?php _e("Responses to your moves"); ?></label></th>
       <td>
@@ -33,6 +43,7 @@ function ivanhoe_save_notification_options( $user_id ) {
     if ( !current_user_can( 'edit_user', $user_id ) )
         return false;
     
+    update_user_meta( $user_id, 'notification_all_moves', $_POST['notification_all_moves'] );
     update_user_meta( $user_id, 'notification_response_moves', $_POST['notification_response_moves'] );
 
 }
