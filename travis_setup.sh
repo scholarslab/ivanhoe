5b0b9418ef1f94f37a889264c406326c0cd2ef6e
Eric Rochester <erochest@virginia.edu>
2015-04-23 15:26:55 -0400

    Create a mysql user for ivanhoe.

--- travis_setup.sh
+++ travis_setup.sh
@@ -4,6 +4,9 @@ curl https://wordpress.org/latest.tar.gz | tar xz
 ln -s $(pwd) wordpress/wp-content/themes/
 cp wordpress/wp-config-sample.php wordpress/wp-config.php
 
-mysql -e 'create database ivanhoe;'      -u root
+mysql -e 'create user ivanhoe;' -u root
+mysql -e 'create database ivanhoe;' -u root
 mysql -e 'create database test_ivanhoe;' -u root
+mysql -e 'grant all privileges on ivanhoe.* to "ivanhoe"@"localhost" identified by "ivanhoe";' -u root
+mysql -e 'grant all privileges on test_ivanhoe.* to "ivanhoe"@"localhost" identified by "ivanhoe";' -u root
 
